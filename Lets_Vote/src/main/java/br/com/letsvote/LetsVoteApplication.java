5fe884f3a4b4c1b894a6c18911b15983f6dc53a3
Felipe Mesquita Uchoa <femeuf5@gmail.com>
2020-02-28 19:55:38 -0300

    setting up the environment

---
+++ Lets_Vote/src/main/java/br/com/letsvote/LetsVoteApplication.java
@@ -0,0 +1,13 @@
+package br.com.letsvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsVoteApplication.class, args);
+	}
+
+}
